92aef347266d4a5f47bf029d2fd3afec900387eb
CarolinaNFeria <rogua20.12@gmail.com>
2025-08-07 14:58:49 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const suits = ["♦", "♥", "♠", "♣"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function getRandomItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function generateCard() {
+  const suit = getRandomItem(suits);
+  const value = getRandomItem(values);
+  const color = (suit === "♥" || suit === "♦") ? "red" : "black";
+
+  
+  
+  document.getElementById("top-suit").textContent = suit;
+  document.getElementById("bottom-suit").textContent = suit;
+
+  document.getElementById("center-value").textContent = value;
+
+  const card = document.getElementById("card");
+  card.classList.remove("red", "black");
+  card.classList.add(color);
+}
+
+document.getElementById("generate").addEventListener("click", generateCard);
+
+
+generateCard();
+}
